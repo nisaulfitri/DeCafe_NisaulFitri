e3afe4ad28e7323c03333d8640e96fb8987362e5
nisaulfitri <nisaulfitri@gmail.com>
2023-10-30 15:01:26 +0700

    connect

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","db_decafe");
+    $conn = mysqli_connect("localhost","u837952942_nisaul","~XHDM2c7","u837952942_nisaul");
     if(!$conn){
         echo "gagal koneksi";
     }
